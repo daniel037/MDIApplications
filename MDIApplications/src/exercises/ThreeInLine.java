6b7ae618888df0da508b3a00c0107d7d8a00a8a1
DANIEL <DANIEL@DANIEL-HP>
2016-10-05 11:42:04 -0500

    faltan validaciones del triqui(3 en linea)

--- MDIApplications/src/exercises/ThreeInLine.java
+++ MDIApplications/src/exercises/ThreeInLine.java
@@ -8,8 +8,11 @@
 package exercises;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,6 +20,9 @@ import javax.swing.JInternalFrame;
  */
 public class ThreeInLine extends JInternalFrame{
     private JButton b[][];
+  
+    private int previus;
+   
     //....................................................
     public ThreeInLine(){
         super("Three in Line",false,true,false,true);
@@ -31,13 +37,36 @@ public class ThreeInLine extends JInternalFrame{
         b=new JButton[3][3];
         for(int r=0;r<3;r++){
             for(int c=0;c<3;c++){
-                b[r][c]=new JButton();
+                b[r][c]=new JButton(" ");
                 add(b[r][c]);
             }
-        }
+        }     
     }
     //..................................................
     private void events(){
-        
+        for(int r=0;r<3;r++)
+        {
+            for(int c=0;c<3;c++)
+            {   
+               b[r][c].addActionListener(new ActionListener() {
+                   @Override
+                   public void actionPerformed(ActionEvent e) {
+                      if(previus == 0)
+                      {
+                          JButton src = (JButton)e.getSource();
+                          src.setText("X");
+                          previus =1;
+                      }
+                      else
+                      {
+                          JButton src = (JButton)e.getSource();
+                          src.setText("O");
+                           previus =0; 
+                      }
+                   }
+               });    
+            }   
+        }
+        //-----------------------------------------------------------------
     }
 }
